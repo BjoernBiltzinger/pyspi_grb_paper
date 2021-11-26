a9c95fbdf7b8fc4fd1ea14f2b291352bf63ee219
BjoernBiltzinger <bjoernbiltzinger@gmx.de>
2021-11-26 14:26:40 +0100

    first real script

---
+++ src/figures/lightcurve.py
@@ -0,0 +1,22 @@
+from pyspi.utils.data_builder.time_series_builder import TimeSeriesBuilderSPI
+import numpy as np
+
+grb_time = "120711 024453" #(YYMMDD HHMMSS) - from GBM trigger time
+background_time_interval_1_spi = "-500--10"
+background_time_interval_2_spi = "150-1000"
+
+tsb_sgl = TimeSeriesBuilderSPI.from_spi_grb("SPIDet0",
+                                            0,
+                                            grb_time,
+                                            ebounds=np.linspace(20,8000,100),
+                                            sgl_type="both",
+                                            poly_order=0,
+                                            )
+
+tsb_sgl.set_background_interval(background_time_interval_1_spi,
+                                background_time_interval_2_spi,
+                                fit_poly=False
+                               )
+
+f = tsb_sgl.view_lightcurve(-300,500);
+f.savefig("lightcurve.pdf")
