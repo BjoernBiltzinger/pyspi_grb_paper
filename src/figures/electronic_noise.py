bf3d734ded666341a9cd91cce980d65051890a32
BjoernBiltzinger <bjoernbiltzinger@gmx.de>
2021-11-26 15:49:25 +0100

    add electronic noise image

---
+++ src/figures/electronic_noise.py
@@ -0,0 +1,43 @@
+from pyspi.utils.data_builder.time_series_builder import TimeSeriesBuilderSPI
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use("matplotlibrc")
+color1 = "#544b3dff"
+color2 = "#340068ff"
+color3 = "#4cb944ff"
+color4 = "#4f7cacff"
+color5 = "#6d2e46ff"
+
+ebounds=np.geomspace(20,8000,100)
+grb_time = "120711 024453" #(YYMMDD HHMMSS) - from GBM trigger time
+
+tsb_psd = TimeSeriesBuilderSPI.from_spi_grb("SPIDet0",
+                                            0,
+                                            grb_time,
+                                            ebounds=ebounds,
+                                            sgl_type="psd",
+                                            poly_order=0,
+                                            )
+
+tsb_non_psd = TimeSeriesBuilderSPI.from_spi_grb("SPIDet0",
+                                            0,
+                                            grb_time,
+                                            ebounds=ebounds,
+                                            sgl_type="sgl",
+                                            poly_order=0,
+                                            )
+
+counts_psd = tsb_psd.time_series.count_per_channel_over_interval(-1000,0)
+counts_non_psd = tsb_non_psd.time_series.count_per_channel_over_interval(-1000,0)
+
+fig, ax = plt.subplots(1,1)
+ax.step(ebounds[1:], counts_psd, label="PSD events", color=color1)
+ax.step(ebounds[1:], counts_non_psd, label="Non-PSD events", color=color2)
+ax.fill_betweenx(np.linspace(0,np.max(counts_non_psd),10),1400, 1900, alpha=0.5, color=color3, label="Main Electronic Noise Region")
+ax.set_yscale("log")
+ax.set_xscale("log")
+ax.set_xlabel("Detected Energy [keV]")
+ax.set_ylabel("Counts")
+ax.legend()
+
+fig.savefig("electronic_noise.pdf")
--- src/figures/lightcurve.py
+++ src/figures/lightcurve.py
@@ -2,6 +2,13 @@ from pyspi.utils.data_builder.time_series_builder import TimeSeriesBuilderSPI
 import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use("matplotlibrc")
+
+color1 = "#544b3dff"
+color2 = "#340068ff"
+color3 = "#4cb944ff"
+color4 = "#4f7cacff"
+color5 = "#6d2e46ff"
+
 grb_time = "120711 024453" #(YYMMDD HHMMSS) - from GBM trigger time
 background_time_interval_1_spi = "-500--10"
 background_time_interval_2_spi = "150-1000"
@@ -20,4 +27,12 @@ tsb_sgl.set_background_interval(background_time_interval_1_spi,
                                )
 
 f = tsb_sgl.view_lightcurve(-300,500);
+
+axis = f.axes[0]
+
+axis.get_lines()[0].set_color(color1)
+axis.get_lines()[1].set_color(color2)
+axis.collections[0].set_color(color3)
+axis.collections[1].set_color(color3)
+axis.legend()
 f.savefig("lightcurve.pdf")
