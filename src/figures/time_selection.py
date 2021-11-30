f646e0a61d595278082859ce52f9b53c6b904528
BjoernBiltzinger <bjoernbiltzinger@gmx.de>
2021-11-30 13:45:57 +0100

    change path

--- src/figures/time_selection.py
+++ src/figures/time_selection.py
@@ -55,11 +55,9 @@ sls1 = []
 tss1 = []
 
 from threeML.utils.data_download.Fermi_GBM.download_GBM_data import download_GBM_trigger_data
-download_GBM_trigger_data("bn120711115", [det], "/home/runner/")
-import os
-print(os.listdir("/home/runner/"))
-tte_file = "/home/runner/glg_tte_{}_bn120711115_v00.fit.gz".format(det)
-rsp2_file="/home/runner/glg_cspec_{}_bn120711115_v00.rsp2".format(det)
+download_GBM_trigger_data("bn120711115", [det], ".")
+tte_file = "glg_tte_{}_bn120711115_v00.fit.gz".format(det)
+rsp2_file="glg_cspec_{}_bn120711115_v00.rsp2".format(det)
 ts = TimeSeriesBuilder.from_gbm_tte('gbm{}'.format(det), tte_file, rsp2_file)
 ts.set_active_time_interval(active_time_gbm)
 ts.set_background_interval(background_time_interval_1_gbm,
