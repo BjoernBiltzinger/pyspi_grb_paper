772b99cd00bc87965855991335c88e5fc786ef79
BjoernBiltzinger <bjoernbiltzinger@gmx.de>
2021-12-02 10:49:07 +0100

    add color

--- src/figures/corner_gbm_pyspi_band.py
+++ src/figures/corner_gbm_pyspi_band.py
@@ -1,6 +1,12 @@
 from chainconsumer import ChainConsumer
 import numpy as np
+plt.style.use("matplotlibrc")
 
+color1 = "#544b3dff"
+color2 = "#340068ff"
+color3 = "#4cb944ff"
+color4 = "#4f7cacff"
+color5 = "#6d2e46ff"
 
 def loadtxt2d(intext):
         try:
@@ -12,11 +18,13 @@ c = ChainConsumer()
 c.configure(plot_hists=False, kde=1.0)
 chain = loadtxt2d('./chains/PYSPI_BAND_post_equal_weights.dat')
 
-c.add_chain(chain[:,:4], parameters=['K', r'$\alpha$', r'$E_{peak}$', r'$\beta$'], name='PYSPI')
+c.add_chain(chain[:,:4], parameters=['K', r'$\alpha$', r'$E_{peak}$', r'$\beta$'],
+            name='PYSPI', color=color1)
 
 chain = loadtxt2d('./chains/GBM_BAND_post_equal_weights.dat')
 
-c.add_chain(chain[:,:4], parameters=['K', r'$\alpha$', r'$E_{peak}$', r'$\beta$'], name='GBM')
+c.add_chain(chain[:,:4], parameters=['K', r'$\alpha$', r'$E_{peak}$', r'$\beta$'], name='GBM',
+            color=color2)
 
 c.configure(plot_hists=False, kde=1.0, shade_alpha=0.5)
 
