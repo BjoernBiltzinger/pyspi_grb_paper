ca8bfd7830da0967015eb79eb2db23eb2efe8ecb
BjoernBiltzinger <bjoernbiltzinger@gmx.de>
2021-12-10 18:28:29 +0100

    add qq and ppc plots

---
+++ src/figures/b0_model_check.py
@@ -0,0 +1,30 @@
+from twopc import PPC
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use("matplotlibrc")
+
+color1 = "#544b3d"
+color2 = "#340068"
+color3 = "#4cb944"
+color4 = "#4f7cac"
+color5 = "#6d2e46"
+
+p = PPC("ppc_output/both_syn_grb.h5")
+
+f = p.detectors["gbmb0"].plot(
+    bkg_subtract=False,
+    levels=[95,68],
+    colors=[color2, color3],
+    lc=color1
+)
+
+f.savefig("b0_ppc.pdf")
+
+f = p.detectors["gbmb0"].plot_qq(
+    channel_energies=[],
+    levels=[95,68],
+    colors=[color2, color3],
+    center_color=color1
+)
+
+f.savefig("b0_qq.pdf")
---
+++ src/figures/psd_13_model_check.py
@@ -0,0 +1,29 @@
+from twopc import PPC
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use("matplotlibrc")
+
+color1 = "#544b3d"
+color2 = "#340068"
+color3 = "#4cb944"
+color4 = "#4f7cac"
+color5 = "#6d2e46"
+
+p = PPC("ppc_output/both_syn_grb.h5")
+
+f = p.detectors["SPIDet13MID"].plot(
+    bkg_subtract=False,
+    levels=[95,68],
+    colors=[color2, color3],
+    lc=color1)
+
+f.savefig("psd_13_ppc.pdf")
+
+f = p.detectors["SPIDet13MID"].plot_qq(
+    channel_energies=[],
+    levels=[95,68],
+    colors=[color2, color3],
+    center_color=color1
+)
+
+f.savefig("psd_13_qq.pdf")
